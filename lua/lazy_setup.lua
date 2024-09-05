93dca768fc0b55dcf0dfe1307797d78ed7268c6e
c-c-k <c-c-k@nym.hush.com>
2024-09-05 10:03:32 +0300

    feat(astronvim): add config files from starter template

---
+++ lua/lazy_setup.lua
@@ -0,0 +1,41 @@
+require("lazy").setup({
+  {
+    "AstroNvim/AstroNvim",
+    -- Remove version tracking to elect for nighly AstroNvim
+    version = "^4",
+    import = "astronvim.plugins",
+    -- AstroNvim options must be set here with the `import` key
+    opts = {
+      -- This ensures the leader key is configured before Lazy is set up
+      mapleader = " ",
+      -- This ensures the localleader key is configured before Lazy is set up
+      maplocalleader = ",",
+      -- Set to false to disable icons (if no Nerd Font is available)
+      icons_enabled = true,
+      -- Default will pin plugins when tracking `version` of AstroNvim, set to
+      -- true/false to override
+      pin_plugins = nil,
+      -- Enable/disable notification about running `:Lazy update` twice to
+      -- update pinned plugins
+      update_notifications = true,
+    },
+  },
+  { import = "community" },
+  { import = "plugins" },
+} --[[@as LazySpec]], {
+  -- Configure any other `lazy.nvim` configuration options here
+  install = { colorscheme = { "astrotheme", "habamax" } },
+  ui = { backdrop = 100 },
+  performance = {
+    rtp = {
+      -- disable some rtp plugins, add more to your liking
+      disabled_plugins = {
+        "gzip",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "zipPlugin",
+      },
+    },
+  },
+} --[[@as LazyConfig]])

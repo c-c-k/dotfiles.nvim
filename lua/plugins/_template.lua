55080317f4c480eed94e7da814dd166ca84846f9
c-c-k <c-c-k@nym.hush.com>
2024-09-12 21:05:14 +0300

    feat(plugins): adjust _template to AstroNvim v4.0

--- lua/plugins/_template.lua
+++ lua/plugins/_template.lua
@@ -1,17 +1,56 @@
 -- ===========================================================================
 -- 
 -- ===========================================================================
+
 -- repo url: <>
 -- nvim help: `:help `
 
+-- == PLUGIN DISABLED ==
+-- MEMO: This is a new plugin config file that needs to be double-checked before enabling.
+if true then return {} end
+
 return {
-  '_plugin_template_placeholder_',
-  -- == PLUGIN DISABLED ==
-  -- MEMO: This is a new plugin config file that needs to be double-checked before enabling.
-  enabled = false,
-  opts = {
-    -- alignment placeholder
+  {
+    '_plugin_id_placeholder_',
+    opts = {
+      -- yank/paste alignment placeholder
+    },
+    config = function(_, opts)      
+      -- yank/paste alignment placeholder
+    end,
   },
-  config = function(_, opts)      
-  end,
+  {
+    "AstroNvim/astrocore",
+    ---@param opts AstroCoreOpts
+    opts = function(_, opts)
+      local astrocore = require "astrocore"
+      local g = {}
+      local maps = astrocore.empty_map_table()
+      local map, mapf = require('cck.utils.config').get_astrocore_mapper(maps)
+
+      opts.autocmds = astrocore.extend_tbl(opts.autocmds, {
+        placeholder_augroup = {
+          {
+            event = { "placeholder_event" },
+            pattern = "placeholder_pattern",
+            desc = "placeholder description",
+            callback = function(args)
+              local maps = astrocore.empty_map_table()
+              local map, mapf = require('cck.utils.config').get_astrocore_mapper(maps)
+
+              map("n", "<LEADER>", "", { desc = "" } )
+
+              astrocore.set_mappings(maps, { buffer = args.data.buf_id })
+            end,
+          }
+        }
+      })
+      map("n", "<LEADER>", "", { desc = "" } )
+
+      g.placeholder = {}
+
+      opts.mappings = astrocore.extend_tbl(opts.mappings, maps)
+      opts.options.g = astrocore.extend_tbl(opts.options.g, g )
+    end,
+  }
 }

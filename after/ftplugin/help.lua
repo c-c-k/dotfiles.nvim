7c2c44dffee45b3d6b06b6da13a312d773bf735a
c-c-k <c-c-k@nym.hush.com>
2024-09-12 14:25:31 +0300

    feat: adjust frplugins to astronvim v4.0

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -1,1 +1,4 @@
+if vim.b.did_ftplugin_cck_help then return end
+vim.b.did_ftplugin_cck_help = true
+
 vim.opt_local.conceallevel = 0
---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,17 @@
+if vim.b.did_ftplugin_cck_markdown then return end
+vim.b.did_ftplugin_cck_markdown = true
+
+vim.opt_local.textwidth = 0
+
+local astrocore = require "astrocore"
+local maps = astrocore.empty_map_table()
+local map, mapf = require('cck.utils.config').get_astrocore_mapper(maps)
+
+map({ "n", "x" }, "<LEADER>gf", "<CMD>CCKGoToFile<CR>", { desc = "(CCK) goto file" } )
+map({ "n", "x" }, "<LEADER>gx", "<CMD>CCKGoToFile<CR>", { desc = "(CCK) goto external" } )
+-- map({ "n", "x" }, "gf", "<CMD>CCKGoToFile<CR>", { desc = "(CCK) goto file" } )
+-- map({ "n", "x" }, "gx", "<CMD>CCKGoToFile<CR>", { desc = "(CCK) goto external" } )
+map("n", "<LEADER>flh", [=[<CMD>FzfBLines ^\v#{1,7}<CR>]=], { desc = "Find(FZF) MD headers (buffer)" } )
+map("n", "<LEADER>fll", [=[<CMD>FzfBLines \v(\[([^ x-]<BAR>[^]]{2,})]<BAR>\<[^>]{2,}\><BAR><https?://)<CR>]=], { desc = "Find(FZF) MD links (buffer)" } )
+
+astrocore.set_mappings(maps, { buffer = true })

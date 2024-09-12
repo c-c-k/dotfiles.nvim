40b63d657ba7414897510e617c3443769179c9d7
c-c-k <c-c-k@nym.hush.com>
2024-09-12 20:56:58 +0300

    feat(plugins): adjust cck-notes to AstroNvim v4.0

--- lua/plugins/cck-notes.lua
+++ lua/plugins/cck-notes.lua
@@ -6,47 +6,50 @@
 -- nvim help: ``
 
 return {
-  -- This pseudo-plugin configuration file is meant for extra notebooks
-  -- related configuration needed for the customization in `lua/cck` and
-  -- `rplugin/.../cck/`.
-  dir = '/dev/null',
-  init = function()
+  "AstroNvim/astrocore",
+  ---@param opts AstroCoreOpts
+  opts = function(_, opts)
+    local astrocore = require "astrocore"
+    local g = {}
+    local maps = astrocore.empty_map_table()
+    local map, mapf = require('cck.utils.config').get_astrocore_mapper(maps)
+
+    map("n", "<LEADER>n", { desc = "Notes" } )
+    map("n", "<LEADER>ne", ":CCKEditNote ", { desc = "Edit note" } )
+    map("n", "<LEADER>nll", ":CCKAddNoteRefLink ", { desc = "Add ref link (input)" } )
+    map("n", "<LEADER>nlc", "<CMD>CCKAddClipboardRefLink<CR>", { desc = "Add ref link (clipboard)" } )
+
+    g.cck_uri_resolvers = { 'cck.pkbm.resolve_uri_as_path' }
+    g.cck_notebook_prefix = 'cck-'
     
-    vim.keymap.set( "n", "<LEADER>ne", ":CCKEditNote ", { desc = "" } )
-    vim.keymap.set( "n", "<LEADER>nll", ":CCKAddNoteRefLink ", { desc = "" } )
-    vim.keymap.set( "n", "<LEADER>nlc", "<CMD>CCKAddClipboardRefLink<CR>", { desc = "" } )
-    
-    vim.g['cck_uri_resolvers'] = {
-    'cck.pkbm.resolve_uri_as_path',
+    g.cck_notebooks = {
+      {
+        name = 'pkb',
+        path = '$NOTEBOOKS_PKB_ROOT',
+        notes_path = '',
+        filename_template = '${TITLE_CLEAN}',
+        templates_path = '$NOTEBOOKS_TEMPLATES',
+        default_template = '$NOTEBOOKS_TEMPLATES/basic_note.tpl',
+      },
+      {
+        name = 'test',
+        path = '$NOTEBOOKS_TEST_NB_ROOT/cck/nb1',
+        notes_path = 'notes',
+        filename_template = '${TITLE_CLEAN}',
+        templates_path = '$NOTEBOOKS_TEMPLATES',
+        default_template = '$NOTEBOOKS_TEMPLATES/basic_note.tpl',
+      },
+      {
+        name = 'test2',
+        path = '$NOTEBOOKS_TEST_NB_ROOT/cck/nb2',
+        notes_path = 'other',
+        filename_template = '%s-${TITLE_CLEAN}',
+        templates_path = '$NOTEBOOKS_TEMPLATES',
+        default_template = '$NOTEBOOKS_TEMPLATES/basic_note.tpl',
+      },
     }
-    vim.g['cck_notebook_prefix'] = 'cck-'
-    
-    vim.g['cck_notebooks'] = {
-    {
-    name = 'pkb',
-    path = '$NOTEBOOKS_PKB_ROOT',
-    notes_path = '',
-    filename_template = '${TITLE_CLEAN}',
-    templates_path = '$NOTEBOOKS_TEMPLATES',
-    default_template = '$NOTEBOOKS_TEMPLATES/basic_note.tpl',
-    },
-    {
-    name = 'test',
-    path = '$NOTEBOOKS_TEST_NB_ROOT/cck/nb1',
-    notes_path = 'notes',
-    filename_template = '${TITLE_CLEAN}',
-    templates_path = '$NOTEBOOKS_TEMPLATES',
-    default_template = '$NOTEBOOKS_TEMPLATES/basic_note.tpl',
-    },
-    {
-    name = 'test2',
-    path = '$NOTEBOOKS_TEST_NB_ROOT/cck/nb2',
-    notes_path = 'other',
-    filename_template = '%s-${TITLE_CLEAN}',
-    templates_path = '$NOTEBOOKS_TEMPLATES',
-    default_template = '$NOTEBOOKS_TEMPLATES/basic_note.tpl',
-    },
-    }
-    
+
+    opts.mappings = astrocore.extend_tbl(opts.mappings, maps)
+    opts.options.g = astrocore.extend_tbl(opts.options.g, g )
   end,
 }

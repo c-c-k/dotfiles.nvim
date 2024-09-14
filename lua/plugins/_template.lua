45a1f374be73c80b5ccfb8735353302d3f95e5bd
c-c-k <c-c-k@nym.hush.com>
2024-09-14 17:46:26 +0300

    feat(plugins): improve plugin config template

--- lua/plugins/_template.lua
+++ lua/plugins/_template.lua
@@ -1,5 +1,5 @@
 -- ===========================================================================
--- 
+--
 -- ===========================================================================
 
 -- repo url: <>
@@ -9,15 +9,25 @@
 -- MEMO: This is a new plugin config file that needs to be double-checked before enabling.
 if true then return {} end
 
+---@type LazySpec
 return {
   {
-    '_plugin_id_placeholder_',
-    opts = {
-      -- yank/paste alignment placeholder
-    },
-    config = function(_, opts)      
-      -- yank/paste alignment placeholder
+    "PLUGIN",
+    -- opts = {
+    --   -- yank/paste alignment placeholder
+    -- },
+    opts = function(_, opts)
+      local astrocore = require "astrocore"
+      return astrocore.extend_tbl(opts, {
+        -- yank/paste alignment placeholder
+      })
     end,
+    -- config = function(plugin, opts)
+    --   -- require("PLUGIN").setup(opts)
+    --   require("astronvim.plugins.configs.PLUGIN")(plugin, opts)
+    --
+    --   -- ...
+    -- end,
   },
   {
     "AstroNvim/astrocore",
@@ -25,8 +35,7 @@ return {
     opts = function(_, opts)
       local astrocore = require "astrocore"
       local g = {}
-      local maps = astrocore.empty_map_table()
-      local map, mapf = require('cck.utils.config').get_astrocore_mapper(maps)
+      local maps, map = require("cck.utils.config").get_astrocore_mapper()
 
       opts.autocmds = astrocore.extend_tbl(opts.autocmds, {
         placeholder_augroup = {
@@ -35,22 +44,23 @@ return {
             pattern = "placeholder_pattern",
             desc = "placeholder description",
             callback = function(args)
-              local maps = astrocore.empty_map_table()
-              local map, mapf = require('cck.utils.config').get_astrocore_mapper(maps)
+              -- selene: allow(shadowing)
+              ---@diagnostic disable-next-line: redefined-local
+              local maps, map = require("cck.utils.config").get_astrocore_mapper()
 
-              map("n", "<LEADER>", "", { desc = "" } )
+              map("n", "<LEADER>", "", { desc = "" })
 
               astrocore.set_mappings(maps, { buffer = args.data.buf_id })
             end,
-          }
-        }
+          },
+        },
       })
-      map("n", "<LEADER>", "", { desc = "" } )
+      map("n", "<LEADER>", "", { desc = "" })
 
       g.placeholder = {}
 
       opts.mappings = astrocore.extend_tbl(opts.mappings, maps)
-      opts.options.g = astrocore.extend_tbl(opts.options.g, g )
+      opts.options.g = astrocore.extend_tbl(opts.options.g, g)
     end,
-  }
+  },
 }

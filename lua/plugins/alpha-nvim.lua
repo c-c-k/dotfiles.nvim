0f109221f8ede8d6f823c8b3127470380c7919f5
c-c-k <c-c-k@nym.hush.com>
2024-09-25 14:00:29 +0300

    feat(plugins): disable alpha-nvim

---
+++ lua/plugins/alpha-nvim.lua
@@ -0,0 +1,12 @@
+-- ==========
+-- ALPHA-NVIM
+-- ==========
+
+-- repo url: <https://github.com/goolord/alpha-nvim>
+-- nvim help: `:help alpa`
+
+---@type LazySpec
+return {
+  "goolord/alpha-nvim",
+  enabled = false,
+}

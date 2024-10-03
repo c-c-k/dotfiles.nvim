5e4bc3f54e43284f1d88f249afafd3e22557249c
c-c-k <c-c-k@nym.hush.com>
2024-10-03 22:09:57 +0300

    feat(plugins/lsp): move bash from astrocommunity

--- lua/community.lua
+++ lua/community.lua
@@ -12,7 +12,6 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.python" },

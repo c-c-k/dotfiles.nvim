8583a6be90d274ec6c8e203776363380bf097639
c-c-k <c-c-k@nym.hush.com>
2024-10-03 22:09:57 +0300

    feat(plugins/lsp): move markdown from astrocommunity

--- lua/community.lua
+++ lua/community.lua
@@ -12,7 +12,6 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.python" },
   -- import/override with your plugins folder
 }

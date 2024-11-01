7edecfb091eb1bf612b773db810b7acfca6f062e
c-c-k <c-c-k@nym.hush.com>
2024-11-01 11:12:25 +0200

    feat(markdown): Set shiftwidth and softtabstop to 2
    Set `shiftwidth` and `softtabstop` to 2 to ensure consistent indentation in markdown files. This improves readability and maintainability.

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,6 +2,8 @@ if vim.b["did_ftplugin_cck_markdown"] then return end
 vim.b["did_ftplugin_cck_markdown"] = true
 
 vim.opt_local.textwidth = 0
+vim.opt_local.shiftwidth = 2
+vim.opt_local.softtabstop = 2
 
 local astrocore = require "astrocore"
 local maps, map = require("cck.utils.config").get_astrocore_mapper()

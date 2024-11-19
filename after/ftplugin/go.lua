3fa539e1a1e00c20d965d16a1f6f44108f259b57
c-c-k <c-c-k@nym.hush.com>
2024-11-19 18:45:51 +0200

    feat(ftplugin): set go files to use width 2 tab (not space)

---
+++ after/ftplugin/go.lua
@@ -0,0 +1,8 @@
+if vim.b["did_ftplugin_cck_go"] then return end
+vim.b["did_ftplugin_cck_go"] = true
+
+vim.opt_local.expandtab = false
+vim.opt_local.textwidth = 0
+vim.opt_local.shiftwidth = 0
+vim.opt_local.softtabstop = 0
+vim.opt_local.tabstop = 2
---
+++ after/ftplugin/gowork.lua
@@ -0,0 +1,8 @@
+if vim.b["did_ftplugin_cck_gowork"] then return end
+vim.b["did_ftplugin_cck_gowork"] = true
+
+vim.opt_local.expandtab = false
+vim.opt_local.textwidth = 0
+vim.opt_local.shiftwidth = 0
+vim.opt_local.softtabstop = 0
+vim.opt_local.tabstop = 2

70f3d87f76ec49dcb391374fd497269f4ca27157
c-c-k <c-c-k@nym.hush.com>
2024-12-01 18:26:15 +0200

    fix(rplugin): GoToEx expands targets

--- rplugin/python3/cck/handle.py
+++ rplugin/python3/cck/handle.py
@@ -2,15 +2,14 @@ from datetime import datetime
 from time import sleep
 
 import pynvim
-
 from cck.uri import URI
 
 
 def handle_uri(vim: pynvim.Nvim, uri: URI):
     temp_log_file = "/dev/shm/vim-gx.log"
     command = (
-            'exe "silent !xdg-open " . shellescape("'
-            f'{uri!s}") . " &>>{temp_log_file}"'
+        'exe "silent !xdg-open " . shellescape("'
+        f'{uri!s}", "#%!") . " &>>{temp_log_file}"'
     )
     with open(temp_log_file, "a") as f:
         f.write(f"--- {datetime.now()}: {command}\n")

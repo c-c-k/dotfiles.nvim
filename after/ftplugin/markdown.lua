80e1cbb05a2a678d206f1156c31d48b748d59c69
c-c-k <c-c-k@nym.hush.com>
2025-01-10 20:37:37 +0200

    feat(markdown): Set shiftwidth and softtabstop to 4

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,8 +2,13 @@ if vim.b["did_ftplugin_cck_markdown"] then return end
 vim.b["did_ftplugin_cck_markdown"] = true
 
 vim.opt_local.textwidth = 0
-vim.opt_local.shiftwidth = 2
-vim.opt_local.softtabstop = 2
+-- set indentation to 4 spaces to:
+-- 1. Fall in line with mkdn-flow's hardcoded indent.
+-- 2. Compensate for mdformat counting sub-indent in ordered lists
+--  from after the last list enumeration digit and thus sees a 2 spaces
+--  sub-indent as being only 1 spaces and therefor flattens the list.
+vim.opt_local.shiftwidth = 4
+vim.opt_local.softtabstop = 4
 
 local astrocore = require "astrocore"
 local maps, map = require("cck.utils.config").get_astrocore_mapper()

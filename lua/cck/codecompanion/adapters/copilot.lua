4b1dd52486601c2090183479c78b0eee87b817be
c-c-k <c-c-k@nym.hush.com>
2025-03-15 13:15:45 +0200

    feat(codecompanion): set Copilot to use Gemini model by default

--- lua/cck/codecompanion/adapters/copilot.lua
+++ lua/cck/codecompanion/adapters/copilot.lua
@@ -1,8 +1,13 @@
 local M = {}
 
-M.copilot_codecompanion = function()
+M.copilot_gem_code_assistant = function()
   return require("codecompanion.adapters").extend("copilot", {
-    name = "copilot_codecompanion",
+    name = "copilot_gem_code_assistant",
+    schema = {
+      model = {
+        default = "gemini-2.0-flash-001",
+      },
+    },
   })
 end
 

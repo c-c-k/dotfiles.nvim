4605b9986cac642569c2c217a0b98d24ca443d28
c-c-k <c-c-k@nym.hush.com>
2025-05-27 13:59:11 +0300

    feat(plugins): add luasnip placeholder

---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,12 @@
+-- =======
+-- LUASNIP
+-- =======
+
+-- repo url: <https://github.com/L3MON4D3/LuaSnip>
+-- nvim help: `:help luasnip.txt`
+
+return {
+  -- PLACEHOLDER
+  -- MEMO: Basic LuaSnip configuration (including blink.nvim integration)
+  --       is done by AstroNvim.
+}

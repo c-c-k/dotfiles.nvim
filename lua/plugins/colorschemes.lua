628bb2ddf098a9cb236594610d110e0c47cccfe9
c-c-k <c-c-k@nym.hush.com>
2025-06-10 08:40:30 +0300

    refactor(plugins): generalize base16-nvim.lua into colorschemes.lua

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,12 @@
+-- ============
+-- COLORSCHEMES
+-- ============
+
+-- NVIM-BASE16
+-- repo url: <https://github.com/RRethy/base16-nvim>
+-- nvim help: `:help base16`
+
+return {
+  { "AstroNvim/astroui", opts = { colorscheme = "base16-solarized-dark" } },
+  { "RRethy/base16-nvim", lazy = true },
+}

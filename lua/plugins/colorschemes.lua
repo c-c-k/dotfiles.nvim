93b2c1cdc2457f103b670e13ae7bba9cbca4741d
c-c-k <c-c-k@nym.hush.com>
2025-06-18 11:29:37 +0300

    feat(plugins): add solarized-osaka and set it as default colorscheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -6,7 +6,22 @@
 -- repo url: <https://github.com/RRethy/base16-nvim>
 -- nvim help: `:help base16`
 
+-- SOLARIZED-OSAKA
+-- repo url: <https://github.com/craftzdog/solarized-osaka.nvim>
+-- nvim help: `:help solarized-osaka.nvim.txt`
+
 return {
-  { "AstroNvim/astroui", opts = { colorscheme = "base16-solarized-dark" } },
+  { "AstroNvim/astroui", opts = { colorscheme = "solarized-osaka" } },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    opts = {
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      dim_inactive = true,
+    },
+  },
   { "RRethy/base16-nvim", lazy = true },
 }

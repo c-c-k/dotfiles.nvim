830fcddb009ac591cc1ca680f221a2e1e457fbc3
c-c-k <c-c-k@nym.hush.com>
2025-11-10 20:57:10 +0200

    feat: merge lua/lazy_setup.lua into root init.lua

--- init.lua
+++ init.lua
@@ -30,4 +30,22 @@ end
 --   own workflow.
 vim.g.usermapleader = " "
 
-require "lazy_setup"
+require("lazy").setup({
+  require "plugins",
+} --[[@as LazySpec]], {
+  -- Configure any other `lazy.nvim` configuration options here
+  install = { colorscheme = { "habamax" } },
+  ui = { backdrop = 100 },
+  performance = {
+    rtp = {
+      -- disable some rtp plugins, add more to your liking
+      disabled_plugins = {
+        "gzip",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "zipPlugin",
+      },
+    },
+  },
+} --[[@as LazyConfig]])

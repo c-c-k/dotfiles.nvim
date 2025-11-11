961d4391ea402a654a996836252f654e355d38a5
c-c-k <c-c-k@nym.hush.com>
2025-11-11 13:28:04 +0200

    fix(ftplugins): override defaults before did_ftplugin due to lazy.nvim bug
    Overriding NVIM defaults needs to be done before the `did_ftplugin`
    checks due to lazy.nvim emitting filetype autocommands multiple times and
    NVIM executing the default filetype config each time, see:
      * https://github.com/folke/lazy.nvim/issues/1993

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -1,4 +1,9 @@
+-- Override NVIM defaults.
+-- Needs to be done before the `did_ftplugin` check due to lazy.nvim emitting
+-- filetype autocommands multiple times and NVIM executing the default filetype
+-- config each time, see:
+--  * https://github.com/folke/lazy.nvim/issues/1993
+vim.opt_local.conceallevel = 0
+
 if vim.b["did_ftplugin_cck_help"] then return end
 vim.b["did_ftplugin_cck_help"] = true
-
-vim.opt_local.conceallevel = 0

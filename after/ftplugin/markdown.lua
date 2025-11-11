bb43afcfa193e3f8513f4073679e3914122ec8a3
c-c-k <c-c-k@nym.hush.com>
2025-11-11 13:28:04 +0200

    feat(ftplugins/markdown): tweak formatting options
    * Enable auto formatting.
    * Restore shiftwidth and softtabstop to global default.

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,14 +1,12 @@
+-- Override NVIM defaults.
+-- Needs to be done before the `did_ftplugin` check due to lazy.nvim emitting
+-- filetype autocommands multiple times and NVIM executing the default filetype
+-- config each time, see:
+--  * https://github.com/folke/lazy.nvim/issues/1993
+vim.opt_local.formatoptions = "tcroqwn2l1j"
+
 if vim.b["did_ftplugin_cck_markdown"] then return end
 vim.b["did_ftplugin_cck_markdown"] = true
-
-vim.opt_local.textwidth = 0
--- set indentation to 4 spaces to:
--- 1. Fall in line with mkdn-flow's hardcoded indent.
--- 2. Compensate for mdformat counting sub-indent in ordered lists
---  from after the last list enumeration digit and thus sees a 2 spaces
---  sub-indent as being only 1 spaces and therefor flattens the list.
-vim.opt_local.shiftwidth = 4
-vim.opt_local.softtabstop = 4
 
 local astrocore = require "astrocore"
 local maps, map = require("cck.core.keymaps").get_astrocore_mapper()

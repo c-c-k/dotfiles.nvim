20e64997abb08c5bdd31ba99d2866e3e45f82bcb
c-c-k <c-c-k@nym.hush.com>
2025-11-14 15:11:31 +0200

    feat(plugins/blink.cmp): Tweak keymaps, path, signature, ghost_text, list

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,16 +3,101 @@ local spec_blink_cmp = {
   "saghen/blink.cmp",
   opts = {
     keymap = {
-      ["<Tab>"] = {
+      ["<CR>"] = {
         function(cmp)
-          if cmp.snippet_active() then return cmp.accept() end
+          if vim.fn.win_gettype() ~= "command" then return cmp.accept() end
         end,
+        "fallback",
+      },
+      ["<C-A>"] = {
+        "select_and_accept",
+        "fallback",
+      },
+      ["<C-Y>"] = {
+        "select_and_accept",
+        "fallback",
+      },
+      ["<C-H>"] = {
+        "show_documentation",
+        "hide_documentation",
+        "show_signature",
+        "hide_signature",
+        "fallback",
+      },
+      ["<C-L>"] = {
+        -- Toggle flag for <C-J> and <C-K> scrolling help or completion list
+        function(cmp)
+          if cmp.is_menu_visible() then
+            vim.b.cck_cmp_scroll_doc = not vim.b.cck_cmp_scroll_doc
+            return true
+          end
+        end,
+        "fallback",
+      },
+      ["<C-J>"] = {
+        function(cmp)
+          if cmp.is_menu_visible() then
+            if vim.b.cck_cmp_scroll_doc then return cmp.scroll_documentation_down(4) end
+          end
+        end,
+        "select_next",
+        "fallback",
+      },
+      ["<C-K>"] = {
+        function(cmp)
+          if cmp.is_menu_visible() then
+            if vim.b.cck_cmp_scroll_doc then return cmp.scroll_documentation_up(4) end
+          end
+        end,
+        "select_prev",
+        "fallback",
+      },
+      ["<Tab>"] = {
+        "select_next",
         "snippet_forward",
         "fallback",
       },
       ["<S-Tab>"] = {
+        "select_prev",
         "snippet_backward",
         "fallback",
+      },
+    },
+    completion = {
+      list = {
+        selection = {
+          preselect = false,
+          auto_insert = true,
+        },
+      },
+      ghost_text = {
+        enabled = true,
+        show_with_selection = false,
+        show_without_selection = true,
+        show_with_menu = true,
+        show_without_menu = true,
+      },
+    },
+    signature = {
+      enabled = true,
+      trigger = {
+        enabled = true,
+        show_on_trigger_character = false,
+        show_on_insert = false,
+        show_on_insert_on_trigger_character = false,
+      },
+    },
+    sources = {
+      providers = {
+        path = {
+          opts = {
+            trailing_slash = false,
+            label_trailing_slash = true,
+            -- get_cwd = function(context) return vim.fn.expand(('#%d:p:h'):format(context.bufnr)) end,
+            show_hidden_files_by_default = true,
+            ignore_root_slash = false,
+          },
+        },
       },
     },
   },
